b6141dd220d60619b87cab3d7a784fb46d970b00
bigzhu <vermiliondun@gmail.com>
2017-11-14 19:26:29 +0800

    可以查出数据

--- db_bz.py
+++ db_bz.py
@@ -2,25 +2,45 @@
 # -*- coding: utf-8 -*-
 
 import configparser
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 
 
 def getDBConf():
     '''
     >>> getDBConf()
-    <Storage...>
+    <Section: db>
     '''
     config = configparser.ConfigParser()
     config.read('conf/db.ini')
     return config['db']
-    # with open('conf/db.ini', 'r') as cfg_file:
-    #    conf.host = config.get('db', 'host')
-    #    conf.port = config.get('db', 'port')
-    #    conf.db_name = config.get('db', 'db_name')
-    #    conf.user = config.get('db', 'user')
-    #    conf.password = config.get('db', 'password')
-    # return conf
+
+
+def getEngine():
+    '''
+    >>> getEngine()
+    Engine(...
+    '''
+
+    db_conf = getDBConf()
+    connect_str = "postgresql+psycopg2://%s:%s@%s:%s/%s" % (
+        db_conf['user'],
+        db_conf['password'],
+        db_conf['host'],
+        db_conf['port'],
+        db_conf['db_name'],
+    )
+    engine = create_engine(connect_str)
+    return engine
+
+
+def query(sql):
+    conn = getEngine().connect()
+    stmt = text("SELECT * FROM oauth_info")
+    print(conn.execute(stmt).fetchall())
 
 
 if __name__ == '__main__':
+    query('123')
     import doctest
     doctest.testmod(verbose=False, optionflags=doctest.ELLIPSIS)
