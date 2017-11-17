34d45ec135d3f7f6a9b568cb06844bff548616d8
bigzhu <vermiliondun@gmail.com>
2017-11-18 00:13:52 +0800

    exp

--- exception_bz.py
+++ exception_bz.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+import traceback
+
+
+def getExpInfoAll(just_info=False):
+    '''
+    得到Exception的异常
+    >>> getExpInfoAll()
+    'NoneType: None...'
+    '''
+    if just_info:
+        info = sys.exc_info()
+        return str(info[1])
+    else:
+        return traceback.format_exc()
+
+
+def getExpInfo():
+    '''
+    得到Exception的异常, 只显示错误信息
+    >>> getExpInfo()
+    'None'
+    '''
+    return getExpInfoAll(True)
+
 
 if __name__ == '__main__':
     import doctest
